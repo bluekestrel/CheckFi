12a036fc1b2623a54d183e0a856c9e144c386a10
bluekestrel <92190404+bluekestrel@users.noreply.github.com>
2021-11-13 19:15:13 -0500

    Initial check smart contract

---
+++ contracts/CheckMinter.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "hardhat/console.sol";
+
+contract CheckMinter is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    struct Check {
+        address checkWriter;
+        address recipient;
+        uint256 amount;
+        bool spendable;
+    }
+
+    address bank;
+    Counters.Counter private _tokenIds;
+    mapping(uint256 => Check) private _checks;
+
+    modifier onlyBank {
+        require(msg.sender == bank);
+        _;
+    }
+
+    constructor() ERC721("CheckFiNFT", "CKFI") {
+        bank = msg.sender;
+    }
+
+    function writeCheck(address writer, address recipient, uint256 amount, string memory tokenURI) external onlyBank returns (uint256) {
+        // The bank mints a new NFT each time a user wants to create a check to send to another person
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        // make a new Check object and then add it to the checks mapping
+        Check memory newCheck = Check(writer, recipient, amount, true);
+        _checks[newItemId] = newCheck;
+
+        return newItemId;
+    }
+
+    function isCheckSpendable(uint256 tokenId) public view returns (bool) {
+        return _checks[tokenId].spendable;
+    }
+
+    function cashCheck(uint256 tokenId, address recipient, uint256 amount) external onlyBank {
+        // While a recipient and amount has been passed, no money is explicitly being transfered
+        // from this contract, the recipient is passed to make sure the requested recipient is the
+        // same one that was listed when the check was 'written' and the amount is passed to make
+        // sure the amount being 'requested' is the same that was requested when the check was
+        // 'written'
+        require(_checks[tokenId].recipient == recipient, "Requested recipient is not original recipeint on check");
+        require(isCheckSpendable(tokenId) == true, "Check has already been spent");
+        require(amount == _checks[tokenId].amount, "Check amount does not match");
+        _checks[tokenId].spendable = false;
+    }
+}
